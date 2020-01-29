4c589d70098173c6c441c1fe9fde0e54bd101d74
aakashkumar1 <aakashkumar1758@gmail.com>
2020-01-29 10:30:11 +0530

    Add files via upload
    unlimited objects spawn script for unity3d Game

---
+++ ObjectsSpawnr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsSpawnr : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] trianglePrefabs;
+    private Vector3 spawnObstaclePosition;
+    // Start is called before the first frame update
+  
+    void Update()
+    {
+        float distanceToHorizon = Vector3.Distance(player.gameObject.transform.position,spawnObstaclePosition);
+        if (distanceToHorizon < 120)
+        {
+            SpawnTriangle();
+        } 
+    }
+    void SpawnTriangle()
+    {
+        spawnObstaclePosition = new Vector3(0, 0, spawnObstaclePosition.z + 25);
+        Instantiate(trianglePrefabs[(Random.Range(0, trianglePrefabs.Length))], spawnObstaclePosition, Quaternion.identity);
+    }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
