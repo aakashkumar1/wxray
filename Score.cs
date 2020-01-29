cc8cf94cf15276b384816b28615f4e9702981285
aakashkumar1 <aakashkumar1758@gmail.com>
2020-01-29 10:32:29 +0530

    Add files via upload
    Complete Score C# Script for unity3d

---
+++ Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Score : MonoBehaviour
+{
+    public int score; 
+    public int highScore;
+    public TextMeshProUGUI scoreUI;
+    public TextMeshProUGUI highScoreUI;
+
+    private void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highscore");
+    }
+    
+         
+    
+    void Update()
+    {
+        scoreUI.text = score.ToString();
+        highScoreUI.text = highScore.ToString();
+        if(score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highscore", highScore);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "score")
+        {
+            score++;
+        }
+    }
+
+}
